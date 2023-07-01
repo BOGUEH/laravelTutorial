7b0dd91f0ba29879637ed57e46f2b5af04a56319
BOGUEH <blessingogueh1@gmail.com>
2023-07-01 19:19:31 +0100

    working with form validation

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,9 +12,13 @@ class LoginController extends Controller
     public function handleLogin(Request $request){
 
             $request->validate([
-                'name' => 'required',
-                'email' => 'required',
-                'password' => 'required'
+
+                'name' => ['required', 'min: 6', 'max: 25', 'alpha'],
+                'email' => ['required', 'email'],
+                'password' => ['required', 'min: 30', 'max: 50']
+
+            ],[
+                'name.alpha' => 'it can only contain alphabet, it will not accept numbers or any other special characters'
             ]);
 
         return $request;
